3e6bc377d35194346ba0da973da49dd95cc71cda
Justin Price <mr.price@Justins-iMac.local>
2020-03-29 10:39:19 -0400

    commented out other cocomponents

--- new-portfolio/src/App.js
+++ new-portfolio/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route} from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+// import Skills from './components/Skills';
+// import Projects from './components/Projects';
+// import ContactForm from './components/ContactForm';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Route exact path="/" component={Home} />
+      {/* <Route path="/Skills" component={Skills} />
+      <Route path="/Projects" component={Projects} />
+      <Route path="/Contact" component={ContactForm} /> */}
     </div>
   );
 }
